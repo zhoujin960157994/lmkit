339c80bca6bddf08ba9c1299aa6389498830c374
Ligin <3077118763@qq.com>
2021-02-07 16:00:14 +0800

    jj
    Signed-off-by: Ligin <3077118763@qq.com>

---
+++ Classes/NSObject+Theme.h
@@ -0,0 +1,18 @@
+//
+//  NSObject+Theme.h
+//  lmkit
+//
+//  Created by lm999 on 2021/2/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (Theme)
+
+-(void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/NSObject+Theme.m
@@ -0,0 +1,17 @@
+//
+//  NSObject+Theme.m
+//  lmkit
+//
+//  Created by lm999 on 2021/2/7.
+//
+
+#import "NSObject+Theme.h"
+
+@implementation NSObject (Theme)
+
+
+-(void)test{
+    NSLog(@"haha");
+}
+
+@end
